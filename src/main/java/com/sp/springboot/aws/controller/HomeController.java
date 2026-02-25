155716165be14bc256935e806315df16ae230916
Shireesha Bandi <shirishabandi46@gmail.com>
2026-02-25 17:34:36 +0530

    small change to test in ec2

--- src/main/java/com/sp/springboot/aws/controller/HomeController.java
+++ src/main/java/com/sp/springboot/aws/controller/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "welcome to AWS first Deployment";
+        return "welcome to AWS first Deployment Shireesha";
     }
 }

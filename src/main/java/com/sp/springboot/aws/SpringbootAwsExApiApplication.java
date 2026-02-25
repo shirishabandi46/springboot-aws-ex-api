646f4b8edb0472dc0834a1981e5559829830961d
Shireesha Bandi <shirishabandi46@gmail.com>
2026-02-25 17:01:19 +0530

    Initial commit - Spring Boot AWS project

---
+++ src/main/java/com/sp/springboot/aws/SpringbootAwsExApiApplication.java
@@ -0,0 +1,13 @@
+package com.sp.springboot.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsExApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsExApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sp/springboot/aws/SpringbootAwsExApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sp.springboot.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsExApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
